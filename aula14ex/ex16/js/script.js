c423a0822d44e50d619dd482f638f7ad53c06f15
Guilherme Henrique <guilhermehenriqueberteli05@gmail.com>
2024-03-04 18:40:26 -0300

    Ex contador de número pronto

--- aula14ex/ex16/js/script.js
+++ aula14ex/ex16/js/script.js
@@ -9,11 +9,22 @@ function contar() {
     saida.innerHTML = ""
 
     if(inicio.value.length == 0 || fim.value.lengt == 0 || passo.value.length == 0) {
-        window.alert("Os dados estão incorretos. Verifique e tente novamente!")
+        window.alert("[ERRO] Os dados estão incorretos. Verifique e tente novamente!")
     } else {
-        for(vInicio;vInicio <= vFim;vInicio += vPasso){
-            saida.innerHTML += `${vInicio} \u{1F449}`
+        if(vPasso == 0) {
+            window.alert("O passo não pode ser 0.Será considerado passo 1.")
+            vPasso = 1
         }
+        if(vInicio < vFim) {
+            for(vInicio;vInicio <= vFim;vInicio += vPasso){
+                saida.innerHTML += `${vInicio} \u{1F449}`
+            }
+        } else {
+            for(vInicio;vInicio >= vFim;vInicio -= vPasso){
+                saida.innerHTML += `${vInicio} \u{1F449}`
+            }
+        }
+        saida.innerHTML += ` \u{1F6A9}`
         if(saida.innerHTML == ""){
             saida.innerHTML = ""
         }
